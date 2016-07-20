21e977c1c8299944ab0abf9cd523f1452739f4c6
Christopher Jenness <christopherjenness@gmail.com>
2016-07-20 05:18:14 -0400

    Add all games

--- scrape_games.py
+++ scrape_games.py
@@ -4,5 +4,9 @@ from urllib.request import urlopen
 page = urlopen('https://github.com/neilmj/BasketballData/tree/master/2016.NBA.Raw.SportVU.Game.Logs').read()
 soup = BeautifulSoup(page)
 
+f = open('allgames.txt', 'w')
 for anchor in soup.findAll('a', class_="js-navigation-open" ):
-    print (anchor)
+    if anchor.text.endswith('.7z') and len(anchor.text)==24:
+        f.write(anchor.text + '\n')
+f.close()
+
