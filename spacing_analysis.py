7d29c6c931ae73aef182d6f45dc886486065cc73
Christopher Jenness <christopherjenness@gmail.com>
2016-07-20 06:10:23 -0400

    Add function to pickle spacing data from game

--- spacing_analysis.py
+++ spacing_analysis.py
@@ -2,22 +2,53 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from scrape import Game
+import pandas as pd
+import pickle
 
-#Extract games from allgames.txt
-games = []
-with open('allgames.txt', 'r') as f:
-    for line in f:
-        game = line.split('.')
-        print(line, game)
-        date = "{game[0]}.{game[1]}.{game[2]}".format(game=game)
-        away = game[3]
-        home = game[5]
-        games.append([date, home, away])
+def extract_games():
+    """
+    Extract games from allgames.txt
+    """
+    games = []
+    with open('allgames.txt', 'r') as f:
+        for line in f:
+            game = line.split('.')
+            date = "{game[0]}.{game[1]}.{game[2]}".format(game=game)
+            away = game[3]
+            home = game[5]
+            games.append([date, home, away])
+    return games
+    
+def get_spacing_statistics(date, home_team, away_team, write_file=False):
+    game = Game(date, home_team, away_team)
+    home_offense_areas, home_defense_areas = [], []
+    away_offense_areas, away_defense_areas = [], []
 
+    for frame in range(len(game.moments)):
+        offensive_team = game.get_offensive_team(frame)
+        if offensive_team == 'home':
+            home_offense_area, away_defense_area = game.get_spacing_area(frame)
+            home_offense_areas.append(home_offense_area)
+            away_defense_areas.append(away_defense_area)
+        if offensive_team == 'away':
+            home_defense_area, away_offense_area = game.get_spacing_area(frame)
+            home_defense_areas.append(home_defense_area)
+            away_offense_areas.append(away_offense_area)
+        if frame % 1000 == 0:
+            print(str(frame) + " / " + str(len(game.moments)))
+    results = (home_offense_areas, home_defense_areas,
+               away_offense_areas, away_defense_areas)
 
+    if write_file:
+        filename = "{date}-{away_team}-{home_team}".format(date=date, away_team=away_team, home_team=home_team)
+        pickle.dump(results,  open( 'data/spacing/' + filename + '.p', "wb"))
 
-test_game = Game('01.06.2016', 'OKC', 'MEM')
+    return(home_offense_areas, home_defense_areas,
+           away_offense_areas, away_defense_areas)
 
+results = get_spacing_statistics('01.06.2016', 'OKC', 'MEM', write_file=True)
+pickle.dump(results,  open( "testr.p", "wb"))
+f = pickle.load(open( "testr.p", "rb" ))
 home_offense_areas, home_defense_areas, away_offense_areas, away_defense_areas = [], [], [], []
 
 for frame in range(len(test_game.moments)):
@@ -28,8 +59,6 @@ for frame in range(len(test_game.moments)):
         away_defense_areas.append(away_defense_area)
     if offensive_team == 'away':
         home_defense_area, away_offense_area = test_game.get_spacing_area(frame)
-        if home_defense_area > 84:
-            print(frame)
         home_defense_areas.append(home_defense_area)
         away_offense_areas.append(away_offense_area)
     if frame % 1000 == 0:
