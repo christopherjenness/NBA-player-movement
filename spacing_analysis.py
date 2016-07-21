ea00009b789808fbe94b74b81b305bf2f82244eb
Christopher Jenness <christopherjenness@gmail.com>
2016-07-21 06:24:24 -0400

    Add documentation

--- spacing_analysis.py
+++ spacing_analysis.py
@@ -8,6 +8,10 @@ import pickle
 def extract_games():
     """
     Extract games from allgames.txt
+    
+    Returns:
+        list: list of games.  Each element is list is [date, home_team, away_team]
+        example element: ['01.01.2016', 'TOR', 'CHI']
     """
     games = []
     with open('allgames.txt', 'r') as f:
@@ -20,6 +24,20 @@ def extract_games():
     return games
     
 def get_spacing_statistics(date, home_team, away_team, write_file=False):
+    """
+    Calculates spacing statistics for each frame in game
+    
+    Args:
+        date (str): date of game in form 'MM.DD.YYYY'.  Example: '01.01.2016'
+        home_team (str): home team in form 'XXX'. Example: 'TOR'
+        away_team (str): away team in form 'XXX'. Example: 'CHI'
+        write_file (bool): If True, write pickle file of statistics into data/spacing directory
+        
+    Returns: 
+        tuple: tuple of data (home_offense_areas, home_defense_areas,
+               away_offense_areas, away_defense_areas), where each element of the tuple
+               is a list of convex hull areas for each frame in the game.
+    """
     game = Game(date, home_team, away_team)
     home_offense_areas, home_defense_areas = [], []
     away_offense_areas, away_defense_areas = [], []
@@ -47,6 +65,19 @@ def get_spacing_statistics(date, home_team, away_team, write_file=False):
            away_offense_areas, away_defense_areas)
            
 def plot_spacing(date, home_team, away_team, defense=True):
+    """
+    Plots team's spacing distrubution in a game.
+    
+    Args:
+        date (str): date of game in form 'MM.DD.YYYY'.  Example: '01.01.2016'
+        home_team (str): home team in form 'XXX'. Example: 'TOR'
+        away_team (str): away team in form 'XXX'. Example: 'CHI'
+        defense (bool): if True, plot defensive spacing.  if False, plot offensive spacing
+        
+    Returns: None
+        Also, shows plt.hist of team spacing during game
+        
+    """
     filename = "{date}-{away_team}-{home_team}".format(date=date, away_team=away_team, home_team=home_team)
     plt.figure()
     if defense:
@@ -59,10 +90,13 @@ def plot_spacing(date, home_team, away_team, defense=True):
     plt.legend(loc='upper right')
     plt.show()
     
-def get_spacing(gamelist):
+def write_spacing(gamelist):
+    """
+    Writes all spacing statistics to data/spacing directory for every game
+    """
     for game in gamelist:
         get_spacing_statistics(game[0], game[1], game[2], write_file=True)
 
 if __name__ == "__main__":
     games = extract_games()
-    get_spacing(games)
+    write_spacing(games)
