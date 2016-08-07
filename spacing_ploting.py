681cb4540fef3f8da214d4998fa6aa701c73833c
Christopher Jenness <christopherjenness@gmail.com>
2016-08-07 10:07:02 -0400

    Move data extraction into function

--- spacing_ploting.py
+++ spacing_ploting.py
@@ -6,42 +6,43 @@ import numpy as np
 from spacing_analysis import extract_games
 import os
 
-test_data = pickle.load(open( "data/spacing/01.10.2016-OKC-POR.p", "rb" ))
-
-plt.scatter(test_data[0], test_data[3])
-
-plt.hist(x=test_data[0], bins = 100, alpha = 0.5)
-plt.hist(x=test_data[2], bins = 100, alpha = 0.5)
-
 
 
     
 game_list = extract_games()
 
-def get_spacing_means(gamelist):
+def get_spacing_details(game):
     """
     Writes all spacing statistics to data/spacing directory for every game
     """
-    means = []
-    for game in gamelist:
-        fname = "{game[0]}-{game[2]}-{game[1]}.p".format(game=game)
-        if fname in os.listdir('data/spacing'):
-            data = pickle.load(open( "data/spacing/"+fname, "rb" ))
-            game_means = tuple(map(lambda x: np.mean(x), data))
-            if game_means[0] > 80:
-                means.append(game_means)
-            else:
-                means.append((game_means[1], game_means[0], game_means[3], game_means[2]))
-        else: print(fname)
-    return means
+    fname = "{game[0]}-{game[2]}-{game[1]}.p".format(game=game)
+    if fname in os.listdir('data/spacing') and fname in os.listdir('data/score'):
+        data = pickle.load(open( "data/spacing/"+fname, "rb" ))
+        score = pickle.load(open( "data/score/"+fname, "rb" )).split(' ')
+        away_points, home_points = score[0], score[2]
+        means = tuple(map(lambda x: np.mean(x), data))
+        if 'GSW' in game:
+            print (game, means, home_points, away_points)
+        return (int(home_points), int(away_points), *means)
+    else:
+        return None
     
-a = get_spacing_means(game_list)
+def get_spacing_df(gamelist):
+    details = []
+    for game in game_list:
+        detail = get_spacing_details(game)
+        if detail:
+            details.append(detail)
+    df = pd.DataFrame(details)
+    df.columns = ['home_points', 'away_points', 'home_offense_areas',
+                         'home_defense_areas', 'away_offense_areas', 'away_defense_areas']
+    return df
+    
+a = get_spacing_df(game_list)
 
-plt.scatter(a[0], a[3])
 
-df = pd.DataFrame(a)
+plt.scatter(a.away_defense_areas, a.home_points)
+plt.xlim(55, 75)
 
-plt.hist(df[0])
-first_half = df[df[0]> 80]
-second_half = df[df[0] < 80]
-plt.scatter(df[0], df[3])
+
+
