d9c46102ee5b901e7dbc7ddfdba5bde2593073c9
Christopher Jenness <christopherjenness@gmail.com>
2016-08-10 07:02:52 -0400

    Add ability to calculate individual player velocity

--- velocity_analysis.py
+++ velocity_analysis.py
@@ -2,9 +2,7 @@
 Analysis of NBA player velocities.
 
 TODO: 
-- Fix spacing on plot
 - individual player velocities
-- Fix all hacks
 - analysis
 """
 
@@ -25,13 +23,31 @@ pickle.dump(game, open('data/game/temp.p', 'wb'))
 
 game = pickle.load(open('data/game/temp.p', 'rb'))
 
-def calculate_velocities(game, frame):
-    if frame == 0:
-        return 0 # Need to figure out what to return here (some sort of zero velocity)
-    details = game._get_moment_details(frame)
+def calculate_velocities(game, frame, highlight_player=None):
+    """
+    
+    """
+    details = game._get_moment_details(frame, highlight_player=highlight_player)
     previous_details = game._get_moment_details(frame - 1)
+    
+    # Highlighed player's edge value (details[8]) is 5 instead of 0.5
+    # Use this fact to retrieve the index of the player
+    
+    print (details[8])
+    
+    if highlight_player:
+        if 5 in details[8]:
+            player_index = details[8].index(5)
+        else:
+            highlight_player=None
+            
+    if frame == 0:
+        if highlight_player:
+            return 0
+        return (0, 0)
+
     if len(details[1]) != 11 or len(details[2]) != 11 or len(previous_details[1]) != 11 or len(previous_details[2]) != 11:
-        return 0 # Is this what I actually want to return?
+        return 0
     delta_x = np.array(details[1]) - np.array(previous_details[1])
     delta_y = np.array(details[2]) - np.array(previous_details[2])
     delta_coordinants = zip(delta_x, delta_y)
@@ -39,6 +55,8 @@ def calculate_velocities(game, frame):
     time_frame = 40 #Fix this value (need to update _get_moment_details to include universe time)
     # Note, universe time is in msec
     velocity = list(map(lambda distances: distances / time_frame, distance_traveled))
+    if highlight_player:
+        return velocity[player_index]
     # Check if home team and away team are assigned correctly
     home_velocity = sum(velocity[1:6])
     away_velocity = sum(velocity[6:])
@@ -48,6 +66,8 @@ def calculate_velocities(game, frame):
     if away_velocity > 0.4:
         away_velocity = 0
     return (home_velocity, away_velocity)
+
+calculate_velocities(game, 10, highlight_player='Nicolas Batum')
 
 def plot_velocity_frame(game, frame_number, ax, highlight_player=None):
     """
@@ -64,6 +84,7 @@ def plot_velocity_frame(game, frame_number, ax, highlight_player=None):
     """
     (game_time, x_pos, y_pos, colors, sizes, quarter, shot_clock, game_clock, edges) = game._get_moment_details(frame_number, highlight_player=highlight_player)
     (commentary_script, score) = game._get_commentary(game_time)
+    
     game._draw_court()
     frame = plt.gca()
     frame.axes.get_xaxis().set_ticks([])
@@ -72,9 +93,9 @@ def plot_velocity_frame(game, frame_number, ax, highlight_player=None):
     plt.xlim(-5, 100)
     plt.ylim(-55, 5)
     sns.set_style('dark')
-    plt.figtext(0.43, 0.125, shot_clock, size=18)
-    plt.figtext(0.5, 0.125, 'Q'+str(quarter), size=18)
-    plt.figtext(0.57, 0.125, str(game_clock), size=18)
+    plt.figtext(0.43, 0.129, shot_clock, size=18)
+    plt.figtext(0.5, 0.129, 'Q'+str(quarter), size=18)
+    plt.figtext(0.57, 0.129, str(game_clock), size=18)
     plt.figtext(0.43, .454, game.away_team + "  " + score + "  " + game.home_team, size=18)
     if highlight_player:
         plt.figtext(0.17, 0.85, highlight_player, size=18)
@@ -83,13 +104,18 @@ def plot_velocity_frame(game, frame_number, ax, highlight_player=None):
                 c=[game.team_colors[game.away_id], game.team_colors[game.home_id]])
 
 
-def watch_play_velocities(game, start_frame, length):
-    end_frame = start_frame + 10 # Need to calculate this properly from length
-    indices = list(range(end_frame - start_frame))
-    home_velocities = [calculate_velocities(game, frame)[0] for frame in range(start_frame, end_frame)]
-    away_velocities = [calculate_velocities(game, frame)[1] for frame in range(start_frame, end_frame)]
+def watch_play_velocities(game, game_time, length, highlight_player=None):
+    
+    starting_frame = game.moments[game.moments.game_time.round() == game_time].index.values[0]
+    ending_frame = game.moments[game.moments.game_time.round() == game_time + length].index.values[0]
+        
+    indices = list(range(ending_frame - starting_frame))
+    home_velocities = [calculate_velocities(game, frame)[0] for frame in range(starting_frame, ending_frame)]
+    away_velocities = [calculate_velocities(game, frame)[1] for frame in range(starting_frame, ending_frame)]
     max_velocity = max(home_velocities + away_velocities)
-    for index, frame in enumerate(range(start_frame, end_frame)):
+    
+    # Plot each frame
+    for index, frame in enumerate(range(starting_frame, ending_frame)):
         f, (ax1, ax2) = plt.subplots(2, figsize=(12,12))
         plot_velocity_frame(game, frame, ax=ax2)
         ax1.set_xlim([0, len(indices)])
@@ -104,7 +130,7 @@ def watch_play_velocities(game, start_frame, length):
         plt.close()
     
     # Make video of each frame
-    command = 'ffmpeg -framerate 20 -start_number 0 -i %d.png -c:v libx264 -r 30 -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" {start_frame}.mp4'.format(start_frame=start_frame)
+    command = 'ffmpeg -framerate 20 -start_number 0 -i %d.png -c:v libx264 -r 30 -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" {starting_frame}.mp4'.format(starting_frame=starting_frame)
     os.chdir('temp')
     os.system(command)
     os.chdir('..')
@@ -115,7 +141,7 @@ def watch_play_velocities(game, start_frame, length):
             os.remove('./temp/{file}'.format(file=file))
 
     
-watch_play_velocities(game, 1000, 1)
+watch_play_velocities(game, 52, 2)
 
 
     
