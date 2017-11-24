bb6cafaaa590c5191bc841c1ee122b61a43bb6f7
Christopher Jenness <christopherjenness@gmail.com>
2017-11-24 07:01:11 -0500

    Organize into main function

--- game/scrape_games.py
+++ game/scrape_games.py
@@ -1,15 +1,22 @@
 """
-Quick scipt to get all games in the data base and save to text file.
+Quick scipt to get all games in the database and save to text file.
 """
 
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
-page = urlopen('https://github.com/sealneaward/nba-movement-data/tree/master/data').read()
-soup = BeautifulSoup(page)
 
-f = open('allgames.txt', 'w')
-for anchor in soup.findAll('a', class_="js-navigation-open" ):
-    if anchor.text.endswith('.7z') and len(anchor.text)==24:
-        f.write(anchor.text + '\n')
-f.close()
+def scrape():
+    page = urlopen(('https://github.com/sealneaward/'
+                    'nba-movement-data/tree/master/data')).read()
+    soup = BeautifulSoup(page)
+    f = open('allgames.txt', 'w')
+    for anchor in soup.findAll('a', class_="js-navigation-open"):
+        if anchor.text.endswith('.7z') and len(anchor.text) == 24:
+            f.write(anchor.text + '\n')
+    f.close()
+    return
+
+
+if __name__ == '__main__':
+    scrape()
